22b316341c7d101b5a0e4c1c19b2236e3096d979
CliffordCheung <cliffordcheung77@gmail.com>
2025-01-22 23:44:12 +0800

    Add output of cart topic arns

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 module "app_topics" {
  source      = "./modules/app_topics"
  name_prefix = "clifford"
+ cart_count = 3
+}
+output "aws_sns_topic" {
+    value = module.app_topics.cart_topic_arns[*]
 }
