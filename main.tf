db69549b5b5d1921b9de8b1094f4927195ec0004
CliffordCheung <cliffordcheung77@gmail.com>
2025-01-22 23:56:58 +0800

    Add module app_topics_john

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
 module "app_topics" {
  source      = "./modules/app_topics"
  name_prefix = "clifford"
- cart_count = 3
+ cart_count = 1
+}
+
+module "app_topics_john" {
+    source = "./modules/app_topics"
+    name_prefix = "john"
+    cart_count = 1
 }
 output "aws_sns_topic" {
     value = module.app_topics.cart_topic_arns[*]
